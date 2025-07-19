485c7d32f9490011e9dbffc6b4bf5542b9e1c108
PuiSan711 <yenps@newhoongfatt.com.my>
2025-07-20 01:58:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import FAISS
-from langchain.document_loaders import DirectoryLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
 import os
 
 app = FastAPI()
